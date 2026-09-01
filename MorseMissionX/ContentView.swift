0b3266d805fc39126a2b61bf8796c4f011a3a770
ShathaMK <shathamukhaild@gmail.com>
2026-09-01 21:28:24 +0300

    Initial Commit

---
+++ MorseMissionX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MorseMissionX
+//
+//  Created by Shatha Almukhaild on 01/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MorseMissionX/MorseMissionXApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorseMissionXApp.swift
+//  MorseMissionX
+//
+//  Created by Shatha Almukhaild on 01/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct MorseMissionXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
